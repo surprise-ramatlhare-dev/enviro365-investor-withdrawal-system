597b18a61abf2498a68d3c8b93d0760d538c5397
Surprise Ramatlhare <ramatlharesurpriserakobo@gmail.com>
2026-06-10 04:25:01 +0200

    Generate Spring Boot backend project

---
+++ backend/enviro365-backend/src/main/java/com/enviro/assessment/junior/surprise/Enviro365BackendApplication.java
@@ -0,0 +1,13 @@
+package com.enviro.assessment.junior.surprise;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Enviro365BackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Enviro365BackendApplication.class, args);
+	}
+
+}
---
+++ backend/enviro365-backend/src/test/java/com/enviro/assessment/junior/surprise/Enviro365BackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.enviro.assessment.junior.surprise;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Enviro365BackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
